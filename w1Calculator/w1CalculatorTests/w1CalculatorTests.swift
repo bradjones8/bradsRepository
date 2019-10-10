b937d065b7c93121142f7ded674a0636d38c43c1
bradjones8 <56162788+bradjones8@users.noreply.github.com>
2019-10-10 10:43:25 -0400

    Add files via upload

---
+++ w1Calculator/w1CalculatorTests/w1CalculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  w1CalculatorTests.swift
+//  w1CalculatorTests
+//
+//  Created by Bradley Jones on 2019-09-06.
+//  Copyright © 2019 Bradley Jones. All rights reserved.
+//
+
+import XCTest
+@testable import w1Calculator
+
+class w1CalculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ wk5Database/View Controllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  wk5Database
+//
+//  Created by Bradley Jones on 2019-10-04.
+//  Copyright © 2019 Bradley Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
+
